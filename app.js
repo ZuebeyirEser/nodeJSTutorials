90d2713247a7730b9356420f34e882af9929f7ba
ZuebeyirEser <zubeyir.eser@icloud.com>
2024-04-02 18:20:47 +0200

    added example of arrow function in js as comment as well as setInterval method

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 console.log("Hello World")
+
+// This is an arrow function in Javascript
+/*
+Traditional anonymous function
+(function (a) {
+  return a + 100;
+});
+
+1. Remove the word "function" and place arrow between the argument and opening body brace
+
+(a) => {
+  return a + 100;
+};
+
+2. Remove the body braces and word "return" — the return is implied.
+(a) => a + 100;
+
+3. Remove the parameter parentheses
+a => a + 100;
+ */
+// execution block within {} is in every 100ms delay.
+setInterval(() =>  {
+    console.log('Hello World')
+}, 100)
