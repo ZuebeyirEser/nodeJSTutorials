fbe3b1055c2fae95acff0381c87ef560509c0f3b
ZuebeyirEser <zubeyir.eser@icloud.com>
2024-04-03 22:15:46 +0200

    added http server

---
+++ 12-http-server.js
@@ -0,0 +1,39 @@
+/* //HTTP set up
+const http = require('http')
+
+const server = http.createServer((req, res)=>{
+    if(req.url === '/'){
+        res.end('Welcome to our home page')
+    }
+    if(req.ulr === '/about') {
+        res.end('here is short history')
+    }
+    res.end(`
+    <h1>Opps!</h1>
+    <p>We cannot find what you are looking for</p>
+    <a href="/">back home</a>
+    `)
+})
+server.listen(4000)
+
+// this code allow us to run http server and keep listening http://localhost:3000/
+ */
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end('<h1>Welcome to our home page</h1>');
+    } else if (req.url === '/about') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end('<h1>Here is a short history</h1>');
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end('<h1>Oops! Page not found</h1>');
+    }
+});
+
+const PORT = 4000;
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}/`);
+});
