bf70ea8558b07f96929e20766bb6e0142fd959d2
ZuebeyirEser <zubeyir.eser@icloud.com>
2024-04-03 11:40:45 +0200

    How to export/import a module in 5-utils.js module.exports and in app.js creating a const using require function
    also 6-alternative.js covers alternative way to export and import module

---
+++ 4-names.js
@@ -0,0 +1,7 @@
+// share
+const hans = "hans";
+const gluck = "gluck";
+ // local   
+const howToNotAccessMe = "how to not access gloabl variable";
+
+module.exports = {hans,gluck};
--- app.js
+++ app.js
@@ -1,25 +1,10 @@
-console.log("Hello World")
 
-// This is an arrow function in Javascript
-/*
-Traditional anonymous function
-(function (a) {
-  return a + 100;
-});
+const names = require('./4-names.js');
+const sayHi = require('./5-utils');
+const data = require('./6-alternative.js');
 
-1. Remove the word "function" and place arrow between the argument and opening body brace
-
-(a) => {
-  return a + 100;
-};
-
-2. Remove the body braces and word "return" — the return is implied.
-(a) => a + 100;
-
-3. Remove the parameter parentheses
-a => a + 100;
- */
-// execution block within {} is in every 100ms delay.
-setInterval(() =>  {
-    console.log('Hello World')
-}, 100)
+console.log(names);
+sayHi(data)
+sayHi('xqc');
+sayHi(names.hans);
+sayHi(names.gluck);
