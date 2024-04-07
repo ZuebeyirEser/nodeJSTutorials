ba84bf3a9ae2da1fe54ba758745dbd3dc84b5963
ZuebeyirEser <zubeyir.eser@icloud.com>
2024-04-07 18:50:15 +0200

    added how to uninstall package 2 different approach

---
+++ 15-uninstall.js
@@ -0,0 +1,9 @@
+// npm uninstall packageName
+// ALternative way is nuclear approach
+// here is the steps:
+/*
+delete node_modules directory
+delete package-log.json file
+from package.json delete the desired package
+npm install
+*/
